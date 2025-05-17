ff08f22ea17aa88ae0ff3b13586eec6067d61d0d
CapybaraSwim <ilya-golovin-03@mail.ru>
2025-05-17 20:49:57 +0300

    Updates

---
+++ 191.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmorze=self.webpackChunkmorze||[]).push([[191],{191:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
